14decd4c555fab1fe4e2890b76bfc9860598b3f4
Deomoreo <deomoreo@gmail.com>
2025-01-16 17:26:43 +0100

    asd

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -1,23 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CatController : MonoBehaviour
 {
-    public float speed = 5f;
-    public float rotationSpeed = 360f;
+    public float walkSpeed = 3f;
+    public float runSpeed = 6f;
+    public float jumpForce = 5f;
+
+    private Animator animator;
+    private Rigidbody rb;
+    private CatInput controls;
+    private Vector2 moveInput;
+    private bool isJumping;
+    private bool isAttacking;
+
+    void Awake()
+    {
+        controls = new CatInput();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        
+        
+        // Bind inputs
+        controls.Move.Newaction.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Move.Newaction.canceled += ctx => moveInput = Vector2.zero;
+        
+
+        controls.Attack.Newaction.performed += _ => Attack();
+
+        controls.Jump.Newaction.performed += _ => Jump();
+
+        controls.FuryMode.Newaction.performed += _ => ActivateFuryMode();
+    }
+
+    void OnEnable() => controls.Enable();
+    void OnDisable() => controls.Disable();
 
     void Update()
     {
-        // Movimento avanti/indietro
-        float move = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        Move();
+    }
 
-        // Rotazione
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+    void Move()
+    {
+        float targetSpeed = moveInput.magnitude > 0 ? (Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed) : 0f;
 
-        // Applica trasformazioni
-        transform.Translate(0, 0, move);
-        transform.Rotate(0, rotation, 0);
+        // Movimento e animazione
+        Vector3 direction = new Vector3(moveInput.x, 0, moveInput.y).normalized;
+        transform.Translate(direction * targetSpeed * Time.deltaTime, Space.World);
+
+        // Aggiorna l'animatore
+        animator.SetFloat("Speed", targetSpeed);
+    }
+
+    void Jump()
+    {
+        if (isJumping) return;
+
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isJumping = true;
+        animator.SetBool("IsJumping", true);
+    }
+    private void Attack()
+    {
+        if (!isAttacking)
+        {
+            isAttacking = true;
+            // Logica di attacco
+            Debug.Log("Attacco eseguito!");
+        }
+    }
+
+    void ActivateFuryMode()
+    {
+        animator.SetBool("IsFuryMode", true);
+        // Logica di modalità furia qui
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+            animator.SetBool("IsJumping", false);
+        }
     }
 }
-
